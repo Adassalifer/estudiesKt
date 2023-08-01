bcb798d3a264c35332fffb08ea9b86af96160c81
Ada Jeanneffer <adassamoda@gmail.com>
2023-07-31 23:03:46 -0300

    variaveis

---
+++ kotlin/firstSteps/Main.kt
@@ -0,0 +1,18 @@
+package firstSteps
+
+fun main () {
+
+    //var counter : Int
+
+    //counter = 10
+
+    //var counter : Int = 10
+
+    var counter = 10
+    counter = 5
+
+    /*Val não pode ser alterado, e quando é um val a palavra não fica sublinhada
+     var as vezes é porta de entrada para bugs */
+
+    val counter2 = 1
+}
